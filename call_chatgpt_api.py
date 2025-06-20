b033de53537f0d05cfa30c1d4b775b4492ece2f0
Yabin Wang <benjaminwang89@gmail.com>
2025-06-20 15:30:06 -0500

    maintain msg history

--- call_chatgpt_api.py
+++ call_chatgpt_api.py
@@ -2,13 +2,43 @@ import os
 
 from openai import OpenAI
 
+EXIT_WORDS = ['exit', 'Exit', 'quit', 'Quit']
+
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-user_input = input("Prompt: ")
+full_input = []
 
-response = client.responses.create(
-    model="gpt-4.1",
-    input=user_input
-)
 
-print(response.output_text)
+while True:
+    user_prompt = input("Prompt: ")
+    print('######\n')
+
+    if user_prompt in EXIT_WORDS:
+        print("User ended conversation.")
+        break
+
+    full_input.append(
+        {
+            "role": "user",
+            "content": [
+                {"type": "input_text",
+                 "text": user_prompt},
+            ]
+        }
+    )
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=full_input
+    )
+    print(f'ChatGPT: {response.output_text}\n######\n')
+
+    response_msg = {
+        "role": "assistant",
+        "content": [
+            {"type": "output_text",
+             "text": response.output_text}
+        ]
+    }
+    full_input.append(response_msg)
+
